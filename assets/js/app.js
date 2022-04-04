f5f700f96e1eb1d4319c09b1d5a82f08f6317d06
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-04-04 16:55:38 +0200

    Mise en place des clusters (début)

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,23 @@ const APIKEY = 'ddc4d27d871e2930d9a8a37809fcb73cefb6bbf6';
 // https://developer.jcdecaux.com/#/opendata/vls?page=getstarted
 const city = 'Rouen';
 
+class Station {
+    constructor(name, number, address, availableBikes, availableStands, totalStands, banking, status, update, marker) {
+        this.name = name;
+        this.number = number;
+        this.address = address;
+        this.availableBikes = availableBikes;
+        this.availableStands = availableStands;
+        this.totalStands = totalStands;
+        this.banking = banking;
+        this.status = status;
+        this.update = update;
+        this.marker = marker;
+    }
+}
+
+let stations = [];
+
 // API JCDecaux
 function callAPI() {
     fetch(`https://api.jcdecaux.com/vls/v1/stations?contract=${city}&apiKey=${APIKEY}`)
@@ -12,68 +29,61 @@ function callAPI() {
         let apiResults = data;
         console.log(apiResults);
 
+        let markersCluster = new L.MarkerClusterGroup();
         for (let i = 0; i < apiResults.length; i++) {
 
-            // Affichage des stations sur la carte
-            let position = apiResults[i].position;
-            marker = L.marker([position.lat, position.lng]).addTo(map);
-            let icons = document.querySelectorAll(".leaflet-marker-icon");
-            if (apiResults[i].available_bikes > 9) {
-                icons[i].src = "./assets/img/marker-icon-green.png";
-            } else if (apiResults[i].available_bikes > 4) {
-                icons[i].src = "./assets/img/marker-icon-orange.png";
-            } else if (apiResults[i].available_bikes > 0) {
-                icons[i].src = "./assets/img/marker-icon-red.png";
+            // Changement du texte de banking
+            if (apiResults[i].banking) {
+                banking = "Oui";
             } else {
-                icons[i].src = "./assets/img/marker-icon-black.png";
+                banking = "Non";
             }
+            // Calcul de la dernière mise à jour
+            let update = '< ' + Math.ceil((new Date () - new Date(apiResults[i].last_update)) / 1000 / 60) + 'min';
+
+            // Création du marker
+            let marker = L.marker([apiResults[i].position.lat, apiResults[i].position.lng]);
+
+            let station = new Station(apiResults[i].name, apiResults[i].number, apiResults[i].address, apiResults[i].available_bikes, apiResults[i].available_bike_stands, apiResults[i].bike_stands, banking, apiResults[i].status, update, marker);
+            stations.push(station);
+            markersCluster.addLayer(stations[i].marker);
+            map.addLayer(markersCluster);
+            
+            let icons = document.querySelectorAll(".leaflet-marker-icon"); --> cibler les marqueurs !
+            // if (apiResults[i].available_bikes > 9) {
+            //     icons[i].src = "./assets/img/marker-icon-green.png";
+            // } else if (apiResults[i].available_bikes > 4) {
+            //     icons[i].src = "./assets/img/marker-icon-orange.png";
+            // } else if (apiResults[i].available_bikes > 0) {
+            //     icons[i].src = "./assets/img/marker-icon-red.png";
+            // } else {
+            //     icons[i].src = "./assets/img/marker-icon-black.png";
+            // }
 
             // Affichage des données
-            icons[i].addEventListener('click', function() {
-                // Changement du texte de banking
-                if (apiResults[i].banking) {
-                    banking = "Oui";
-                } else {
-                    banking = "Non";
-                }
-                // Calcul de la dernière mise à jour
-                let update = '< ' + Math.ceil((new Date () - new Date(apiResults[i].last_update)) / 1000 / 60) + 'min';
+            // icons[i].addEventListener('click', function() {
 
-                let station = new Station(apiResults[i].name, apiResults[i].number, apiResults[i].address, apiResults[i].available_bikes, apiResults[i].available_bike_stands, apiResults[i].bike_stands, banking, apiResults[i].status, update);
+            //     let station = new Station(apiResults[i].name, apiResults[i].number, apiResults[i].address, apiResults[i].available_bikes, apiResults[i].available_bike_stands, apiResults[i].bike_stands, banking, apiResults[i].status, update);
 
-                document.querySelector('.map__infos__name').textContent = station.name.split('-')[1];
-                document.querySelector('.map__infos__number').textContent = station.number;
-                // document.querySelector('.map__infos__address').textContent = station.address;
-                document.querySelector('.map__infos__available-bikes').textContent = station.availableBikes;
-                document.querySelector('.map__infos__available-stands').textContent = station.availableStands;
-                document.querySelector('.map__infos__total-stands').textContent = station.totalStands;
-                document.querySelector('.map__infos__banking').textContent = station.banking;
-                // Affichage du statut en couleur
-                if (station.status === "OPEN") {
-                    document.querySelector('.map__infos__status').textContent = 'Ouvert';
-                    document.querySelector('.map__infos__status').style.color = '#4BB543';
-                } else {
-                    document.querySelector('.map__infos__status').textContent = 'Fermé';
-                    document.querySelector('.map__infos__status').style.color = '#ff3333';
-                }
-                document.querySelector('.map__infos__update').textContent = station.update;
-            })
+            //     document.querySelector('.map__infos__name').textContent = station.name.split('-')[1];
+            //     document.querySelector('.map__infos__number').textContent = station.number;
+            //     // document.querySelector('.map__infos__address').textContent = station.address;
+            //     document.querySelector('.map__infos__available-bikes').textContent = station.availableBikes;
+            //     document.querySelector('.map__infos__available-stands').textContent = station.availableStands;
+            //     document.querySelector('.map__infos__total-stands').textContent = station.totalStands;
+            //     document.querySelector('.map__infos__banking').textContent = station.banking;
+            //     // Affichage du statut en couleur
+            //     if (station.status === "OPEN") {
+            //         document.querySelector('.map__infos__status').textContent = 'Ouvert';
+            //         document.querySelector('.map__infos__status').style.color = '#4BB543';
+            //     } else {
+            //         document.querySelector('.map__infos__status').textContent = 'Fermé';
+            //         document.querySelector('.map__infos__status').style.color = '#ff3333';
+            //     }
+            //     document.querySelector('.map__infos__update').textContent = station.update;
+            // })
         }
     })
-}
-
-class Station {
-    constructor(name, number, address, availableBikes, availableStands, totalStands, banking, status, update) {
-        this.name = name;
-        this.number = number;
-        this.address = address;
-        this.availableBikes = availableBikes;
-        this.availableStands = availableStands;
-        this.totalStands = totalStands;
-        this.banking = banking;
-        this.status = status;
-        this.update = update;
-    }
 }
 
 callAPI();
